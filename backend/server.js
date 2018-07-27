c50749be0db3fb1a1ce9d1dfc67313cd0d0e1955
daniel.caterson <daniel.caterson@gmail.com>
2018-07-26 22:40:53 -0400

    ready for deplyoment

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,19 @@
 // server.js
-
-// first we import our dependencies…
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import sqlite from 'sqlite3';
 import path from 'path';
+import dotenv from 'dotenv';
 
-//import Comment from '../client/build/';
-
-// and create our instances
 const app = express();
 const router = express.Router();
+const result = dotenv.config()
 
-// set our port to either a predetermined port number if you have set it up, or 3001
-
-console.log('ENV: '+ process.env.DEV_ENV);
-console.log('PORT: '+ process.env.PORT);
-console.log('ENV: '+ process.env.DEV_ENV);
-// now we should configure the API to use bodyParser and look for JSON data in the request body
+// configure the API to use bodyParser and look for JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger('dev'));
-
-
-
-
-router.get('/', (req, res) => {
-  console.log('great');
-  res.json({ message: 'API Is Perfectly Adequate' });
-  res.send({message: 'API Is Perfectly Adequate'});
-});
-
-
-
-
-
-// now we can set the route path & initialize the API
 
 // Connect to DB
 let db = new sqlite.Database('./db/nanourl.db', (err) => {
@@ -94,35 +71,16 @@ router.get('/redirect/:id', (req,res) =>{
   })
 });
 
-
-//sqlite test
-router.get('/lites', (req,res) =>{
-  db.all('SELECT * FROM links', (err, result) => {
-    if(!err)
-        return res.json({ success: true, result: result});
-      else
-        return res.json({ success: false, error: err }); 
-
-  });  
-});
-
 app.use('/api', router);
 var API_PORT = 3001;
-if(process.env.NODE_ENV=="production"|| true){
-  const API_PORT = 3000;
+if(process.env.NODE_ENV=="production"|| 1){
+  API_PORT = 3000;
   app.use( express.static(path.resolve(__dirname, '../client/build') ) );
   app.get('*', (req, res)=>{
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   });
 
 }
-
-/*
-/api will be included as a variable in any page/component that needs it 
-figure out how to use secrets and .env
-
-*/
-
 
 // Use our router configuration when we call /api
 //http://nephewapps.com/2018/05/10/handling-dev-vs-production-api-calls-using-create-react-app/
